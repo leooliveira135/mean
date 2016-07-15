b328bac659329383daed4b899dc0373b7b3bd760
leooliveira135 <oliveiraleonardo61@gmail.com>
2016-07-15 16:02:13 -0300

    Alteração de arquivos de configuração

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 // config/config.js
 
-module.exports = {
-    var processo = require('env/' + process.env.NODE_ENV + '.js');
-    return processo;
+module.exports = function () {
+    return require('./env/' + process.env.NODE_ENV + '.js');
 }
